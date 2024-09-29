a34ae99827408bec6b31be3029a4fc76d2884944
Xurliman <jumamuratovahurliman8@gmail.com>
2024-09-29 14:51:41 +0500

    first increment

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strings"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	http.HandleFunc("/update/counter/", webhook)
+	http.HandleFunc("/update/gauge/", webhook)
+	return http.ListenAndServe(`:8080`, nil)
+}
+
+func webhook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	path := r.URL.Path
+	nameAndVal := strings.ReplaceAll(path, "/update/counter/", "")
+	if nameAndVal == "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	return
+}
