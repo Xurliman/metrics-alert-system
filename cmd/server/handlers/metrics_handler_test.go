9ccea0c0367a0ba14727f7868459172bbedf1c42
Xurliman <jumamuratovahurliman8@gmail.com>
2024-09-30 16:30:00 +0500

    created a test file for metric handler

---
+++ cmd/server/handlers/metrics_handler_test.go
@@ -0,0 +1,70 @@
+package handlers
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestUpdateMetrics(t *testing.T) {
+	type expected struct {
+		statusCode int
+	}
+	tests := []struct {
+		name     string
+		method   string
+		url      string
+		expected expected
+	}{
+		{
+			name:   "first",
+			method: http.MethodPost,
+			url:    "http://localhost:8080/update/counter/someMetric/527",
+			expected: expected{
+				statusCode: http.StatusOK,
+			},
+		},
+		{
+			name:   "second",
+			method: http.MethodGet,
+			url:    "http://localhost:8080/update/counter/someMetric/527",
+			expected: expected{
+				statusCode: http.StatusMethodNotAllowed,
+			},
+		},
+		{
+			name:   "third",
+			method: http.MethodPost,
+			url:    "http://localhost:8080/update/counter//527",
+			expected: expected{
+				statusCode: http.StatusNotFound,
+			},
+		},
+		{
+			name:   "fourth",
+			method: http.MethodPost,
+			url:    "http://localhost:8080/update/unknown/someMetric/527",
+			expected: expected{
+				statusCode: http.StatusNotImplemented,
+			},
+		},
+		{
+			name:   "fifth",
+			method: http.MethodPost,
+			url:    "http://localhost:8080/update/counter/someMetric/unknown",
+			expected: expected{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			client := &http.Client{}
+			request, err := http.NewRequest(test.method, test.url, nil)
+			assert.NoError(t, err)
+			response, err := client.Do(request)
+			assert.NoError(t, err)
+			assert.Equal(t, test.expected.statusCode, response.StatusCode)
+		})
+	}
+}
